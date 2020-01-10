917b9f5365d4c8fb53706e8ea901929e4a02c5f0
Dergust <a_potapov_86@mail.ru>
2020-01-10 22:27:57 +0300

    greedy search was added

---
+++ examples.py
@@ -0,0 +1,36 @@
+from sklearn import datasets
+from sklearn.model_selection import StratifiedKFold
+from features_selection import *
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import make_scorer, roc_auc_score
+
+
+def full_search_test():
+    data = datasets.load_breast_cancer()
+    X, y = data.data[:, :12], data.target
+    cv = StratifiedKFold(n_splits=5, shuffle=True)
+    model = LogisticRegression(C=1, solver='liblinear')
+    scorer = make_scorer(roc_auc_score)
+    full_search = FullSearch(X, y, model=model, scorer=scorer, cv=cv, verbose=True)
+    print(full_search.run())
+
+def greedy_search_test():
+    data = datasets.load_breast_cancer()
+    X, y = data.data[:, :], data.target
+    cv = StratifiedKFold(n_splits=5, shuffle=True)
+    model = LogisticRegression(C=1, solver='liblinear')
+    scorer = make_scorer(roc_auc_score)
+    greedy_search = GreedySearch(X, y, model=model, scorer=scorer, cv=cv, forward=True, verbose=True)
+    print(greedy_search.run())
+
+def clear_test():
+    data = datasets.load_breast_cancer()
+    X, y = data.data[:, :], data.target
+    cv = StratifiedKFold(n_splits=5, shuffle=True)
+    model = LogisticRegression(C=1, solver='liblinear')
+    scorer = make_scorer(roc_auc_score)
+    score = cross_val_score(model, X, y, cv=cv, scoring=scorer, n_jobs=-1)
+    print(score.mean(), score.std())
+
+if __name__ == "__main__":
+    greedy_search_test()
